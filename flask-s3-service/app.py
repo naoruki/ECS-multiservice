a1c786d9107288d1141f795ead9ffbaaaa63ddf5
Jazeel <jazeel@sph.com.sg>
2025-02-13 10:36:53 +0800

    Remove ddb

--- flask-s3-service/app.py
+++ flask-s3-service/app.py
@@ -53,4 +53,4 @@ def upload_file():
         return f"Error uploading file: {str(exc)}", 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5003)
+    app.run(host="0.0.0.0", port=5001)
